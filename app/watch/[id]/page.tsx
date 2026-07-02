0865307f9ecb0533e3bdfdeb6f1c8a5df8cd64c4
niatube <fred.mavinga@niamediagroupinc.com>
2026-07-02 14:23:48 -0400

    Remove nested useEffect from watch page

--- app/watch/[id]/page.tsx
+++ app/watch/[id]/page.tsx
@@ -225,18 +225,6 @@ useEffect(() => {
   loadMonetizationPresets(profile.currencyCode);
 }, [viewerCountry]);
 
- useEffect(() => {
-  if (typeof window === "undefined") return;
-
-  const locale = navigator.language;
-  const detectedCountry = getCountryFromBrowserLocale(locale);
-  const context = resolveSupportContext(detectedCountry ?? undefined);
-
-  setViewerCountry(context.country);
-  setViewerCurrency(context.currencyCode);
-
-  console.log("Support Context:", context);
-}, []);
  
   useEffect(() => {
     async function loadVideo() {
